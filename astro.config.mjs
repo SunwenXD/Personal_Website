eef177a5352bad1562c1f5df2b4e8716882ab36c
Sunwen0625 <aagg50211@gmail.com>
2026-07-22 20:22:34 +0800

    新增：頭像、技術棧圖標、多益計分動畫、頁面優化

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://SunwenXD.github.io',
   base: '/Personal_Website',
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   }
